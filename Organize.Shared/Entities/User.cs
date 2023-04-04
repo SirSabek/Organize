b5b559ab7ae6d8dffd46692349c7028cb09e8c1b
SirSabek <mmsabek99@gmail.com>
2023-04-04 12:20:00 +0200

    adding the first entity

---
+++ Organize.Shared/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Organize.Shared.Entities
+{
+    public class User
+    {
+        [Required]
+        [StringLength(15,ErrorMessage = "Username is too long!")]
+        public string Username { get; set; }
+        
+        [Required(ErrorMessage = "The Password is required!")]
+        public string Password { get; set; }
+    }
+} 
