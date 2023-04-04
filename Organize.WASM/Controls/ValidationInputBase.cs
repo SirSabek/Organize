74aaf62421923efc5c39097dedc08d56ce2454ad
SirSabek <mmsabek99@gmail.com>
2023-04-04 12:20:46 +0200

    Sign In page validation

--- Organize.WASM/Controls/ValidationInputBase.cs
+++ Organize.WASM/Controls/ValidationInputBase.cs
@@ -10,6 +10,12 @@ public class ValidationInputBase : ComponentBase
     [Parameter]
     public string Value { get; set; }
     
+    [Parameter]
+    public string Error { get; set; }
+    
+    [Parameter(CaptureUnmatchedValues = true)]
+    public Dictionary<string,object> InputAttributes { get; set; }
+    
     protected async void OnValueChanged(ChangeEventArgs e)
     {
        await ValueChanged.InvokeAsync(e.Value.ToString());
---
+++ Organize.WASM/Pages/SignInBase.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq.Expressions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Organize.Shared.Entities;
+
+namespace Organize.WASM.Pages;
+
+public class SignInBase : ComponentBase
+{
+   protected string DayOfWeek { get;} = DateTime.Now.DayOfWeek.ToString();
+   protected string Username { get; set; } = "ben";
+   protected User User { get; set; }
+   protected EditContext EditContext { get; set; }
+
+   public SignInBase()
+   {
+       User = new User();
+   }
+
+   protected override void OnInitialized()
+   {
+       base.OnInitialized();
+       EditContext = new EditContext(User);
+   }
+
+   protected void HandleUserNameChanged(ChangeEventArgs e)
+   {
+        Username = e.Value.ToString();
+   }
+   
+   protected void HandelUserNameValueChanged(string value)
+   {
+        Username = value;
+   }
+
+   public string GetError(Expression<Func<object>> fu)
+   {
+        return EditContext == null ? null : EditContext.GetValidationMessages(fu).FirstOrDefault();
+   }
+}
