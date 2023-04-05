6c25f9e7cfe7da3e25e73c05e4bbbdefac2b9c1e
SirSabek <mmsabek99@gmail.com>
2023-04-05 16:10:52 +0200

    add navigation functionality

--- Organize.WASM/Pages/SignUpBase.cs
+++ Organize.WASM/Pages/SignUpBase.cs
@@ -1,32 +1,53 @@
 ﻿using GeneralUI.DropdownControl;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Primitives;
 using Organize.Shared.Enums;
 
 namespace Organize.WASM.Pages;
 
 public class SignUpBase : SignBase
 {
- protected IList<DropdownItem<GenderTypeEnum>> GenderTypeDropdownItems { get; } = new List<DropdownItem<GenderTypeEnum>>();
- protected DropdownItem<GenderTypeEnum> selectedGenderTypeDropdownItem { get; set; }
+     [Inject]
+     private NavigationManager NavigationManager { get; set; }
+     protected IList<DropdownItem<GenderTypeEnum>> GenderTypeDropdownItems { get; } = new List<DropdownItem<GenderTypeEnum>>();
+     protected DropdownItem<GenderTypeEnum> selectedGenderTypeDropdownItem { get; set; }
 
- protected override void OnInitialized()
- {
-  base.OnInitialized();
+     protected override void OnInitialized()
+     { 
+         base.OnInitialized();
 
-  var male = new DropdownItem<GenderTypeEnum>()
-  {
-   itemObject = GenderTypeEnum.Male,
-   DisplayText = "Male"
-  };
+         var male = new DropdownItem<GenderTypeEnum>()
+         { 
+             itemObject = GenderTypeEnum.Male,
+             DisplayText = "Male"
+         };
+         var female = new DropdownItem<GenderTypeEnum>()
+         {
+              itemObject = GenderTypeEnum.Female,
+              DisplayText = "Female"
+         };
 
-  var female = new DropdownItem<GenderTypeEnum>()
-  {
-   itemObject = GenderTypeEnum.Female,
-   DisplayText = "Female"
-  };
+         GenderTypeDropdownItems.Add(male);
+         GenderTypeDropdownItems.Add(female);
 
-  GenderTypeDropdownItems.Add(male);
-  GenderTypeDropdownItems.Add(female);
+         selectedGenderTypeDropdownItem = male;
+         TryGetUsernameFromUri();
+     }
 
-  selectedGenderTypeDropdownItem = male;
- }
+
+     public void TryGetUsernameFromUri()
+     {
+         var uri = NavigationManager.ToAbsoluteUri(NavigationManager.Uri);
+         StringValues sv;
+         if (QueryHelpers.ParseQuery(uri.Query).TryGetValue("username", out sv))
+         {
+            User.Username = sv;
+         }
+
+     }
+     public void OnValidSubmit()
+     {
+         NavigationManager.NavigateTo("signin");
+     }
 }
