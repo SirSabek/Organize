28ea186cbfb46776676b4904f15d06c1eb066007
SirSabek <mmsabek99@gmail.com>
2023-04-06 14:43:58 +0200

    adding UserManager class

--- Organize.Business/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Organize.Business
-{
-    public class Class1
-    {
-    }
-}
---
+++ Organize.Business/UserManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Organize.Shared.Entities;
+
+namespace Organize.Business
+{
+    public class UserManager
+    {
+        public async Task<User> TrySignInAndGetUserAsync(User user)
+        {
+            return await Task.FromResult(new User());
+        }
+
+        public async Task InsertUserAsync(User user)
+        {
+            await Task.FromResult(true);
+        }
+    }
+}
