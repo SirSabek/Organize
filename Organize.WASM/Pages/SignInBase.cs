da77ae5b9ef20fc83ac5b4960ab4ef8c1a217efa
SirSabek <mmsabek99@gmail.com>
2023-04-06 14:44:22 +0200

    updating signin

--- Organize.WASM/Pages/SignInBase.cs
+++ Organize.WASM/Pages/SignInBase.cs
@@ -1,10 +1,31 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Organize.Business;
 
 namespace Organize.WASM.Pages;
 
 public class SignInBase : SignBase
 {
    protected string DayOfWeek { get;} = DateTime.Now.DayOfWeek.ToString();
+
+    [Inject]
+    private NavigationManager NavigationManager { get; set; }
+
+   protected async void OnSubmit()
+   {
+       if (EditContext.Validate())
+       {
+           return;
+           
+       }
+
+       var userManager = new UserManager();
+       var user = await userManager.TrySignInAndGetUserAsync(User);
+       if (user != null)
+       {
+            NavigationManager.NavigateTo("item");
+       }
+   }
+   
    // protected string Username { get; set; } = "ben";
    //
    //
